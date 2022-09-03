1aba1cc14759ca336a5bdbda81f30a8272e8ec4f
john-mirage <chaosdynamix@posteo.net>
2022-09-03 15:12:21 +0200

    feat: handle basic password display

--- src/components/app-password-display.ts
+++ src/components/app-password-display.ts
@@ -1,17 +1,40 @@
 import resetSheet from "@styles/reset";
 
 class AppPasswordDisplay extends HTMLElement {
+  passwordElement: HTMLParagraphElement;
+
+  static get observedAttributes() {
+    return ["value"];
+  }
+
   constructor() {
     super();
     const template = <HTMLTemplateElement>document.getElementById("template-app-password-display");
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.adoptedStyleSheets = [resetSheet];
     shadowRoot.append(template.content.cloneNode(true));
+    this.passwordElement = <HTMLParagraphElement>shadowRoot.querySelector("#password");
   }
 
-  connectedCallback() {
-
+  get value(): string {
+    return this.getAttribute("value") || "P4$5W0rD!";
   }
+
+  set value(newValue: string) {
+    this.setAttribute("value", newValue);
+    this.passwordElement.classList.add("display__password--generated");
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    switch (name) {
+      case "value":
+        this.passwordElement.textContent = newValue;
+        break;
+      default:
+        throw new Error("The modified attribute is not observed");
+    }
+  }
+
 }
 
 export default AppPasswordDisplay;
--- src/components/app-password-generator.ts
+++ src/components/app-password-generator.ts
@@ -1,8 +1,12 @@
 import resetSheet from "@styles/reset";
 import AppPasswordDisplay from "@components/app-password-display";
 
+const alphaDatabase = "abcdefghijklmnopqrstuvwxyz";
+const numberDatabase = "0123456789";
+const symbolDatabase = "&é(-è_çà)=$ù%";
+
 class AppPasswordGenerator extends HTMLElement {
-  displayElement: AppPasswordDisplay
+  displayElement: AppPasswordDisplay;
   
   constructor() {
     super();
@@ -11,16 +15,41 @@ class AppPasswordGenerator extends HTMLElement {
     shadowRoot.adoptedStyleSheets = [resetSheet];
     shadowRoot.append(template.content.cloneNode(true));
     this.displayElement = <AppPasswordDisplay>shadowRoot.querySelector("#display");
-    this.generatePassword = this.generatePassword.bind(this);
+    this.handleGeneratePassword = this.handleGeneratePassword.bind(this);
   }
 
   connectedCallback() {
-    this.addEventListener("generate-password", this.generatePassword);
+    this.addEventListener("generate-password", this.handleGeneratePassword);
   }
 
-  generatePassword(event: Event) {
+  disconnectedCallback() {
+    this.removeEventListener("generate-password", this.handleGeneratePassword);
+  }
+
+  handleGeneratePassword(event: Event) {
     const { length, hasUppercase, hasLowercase, hasNumbers, hasSymbols } = (<CustomEvent>event).detail;
-    console.log("generate");
+    this.generatePassword(length, hasUppercase, hasLowercase, hasNumbers, hasSymbols);
+  }
+
+  generatePassword(
+    length: number,
+    hasUppercase: boolean,
+    hasLowercase: boolean,
+    hasNumbers: boolean,
+    hasSymbols: boolean
+  ) {
+    let database = "";
+    if (hasUppercase) database += alphaDatabase.toUpperCase();
+    if (hasLowercase) database += alphaDatabase.toLowerCase();
+    if (hasNumbers) database += numberDatabase;
+    if (hasSymbols) database += symbolDatabase;
+    console.log("generate...");
+    let password = "";
+    for (let index = 0; index < length; ++index) {
+      password += database.charAt(Math.random() * database.length);
+    }
+    console.log(password);
+    this.displayElement.value = password;
   }
 }
 
